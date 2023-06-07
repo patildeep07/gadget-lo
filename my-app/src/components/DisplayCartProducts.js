0d5a4854c0eafa5793acbe359a51f9288f6d7230
Deep Patil <59699631+patildeep07@users.noreply.github.com>
2023-06-07 15:39:09 +0530

    Added img aside, on CART and WISHLIST page

--- my-app/src/components/DisplayCartProducts.js
+++ my-app/src/components/DisplayCartProducts.js
@@ -2,8 +2,7 @@ import { useContext } from "react";
 import { AppContext } from "../context/AppProvider";
 
 export const DisplayCartProducts = ({ product }) => {
-  const { title, categoryName, price, rating, inCart, inWishlist, qty, _id } =
-    product;
+  const { title, thumbnail, price, qty, _id } = product;
   const {
     handleQuantity,
     removeFromCart,
@@ -13,41 +12,44 @@ export const DisplayCartProducts = ({ product }) => {
     isInWishlist,
   } = useContext(AppContext);
   return (
-    <div
-      style={{ border: "1px solid lightgray", margin: "10px", padding: "10px" }}
-    >
-      <h1>{title}</h1>
-      <h3>Price: INR {price}</h3>
-      <p>
-        <button
-          onClick={() =>
-            qty > 1
-              ? handleQuantity(product._id, "decrement")
-              : removeFromCart(product._id, product)
-          }
-        >
-          -
-        </button>{" "}
-        QTY: {qty}{" "}
-        <button onClick={() => handleQuantity(product._id, "increment")}>
-          +
-        </button>
-      </p>
-      {!isInCart(_id) && <button>Add to cart</button>}
-      {isInCart(_id) && (
-        <button onClick={() => removeFromCart(product._id, product)}>
-          Remove from cart
-        </button>
-      )}
+    <div className="flex-row-space-evenly">
+      <img src={thumbnail}></img>
+      <div>
+        <h1>{title}</h1>
+        <h3>Price: INR {price}</h3>
+        <p>
+          <button
+            onClick={() =>
+              qty > 1
+                ? handleQuantity(product._id, "decrement")
+                : removeFromCart(product._id, product)
+            }
+          >
+            -
+          </button>{" "}
+          QTY: {qty}{" "}
+          <button onClick={() => handleQuantity(product._id, "increment")}>
+            +
+          </button>
+        </p>
+        {!isInCart(_id) && <button>Add to cart</button>}
+        {isInCart(_id) && (
+          <button onClick={() => removeFromCart(product._id, product)}>
+            Remove from cart
+          </button>
+        )}
 
-      {!isInWishlist(_id) && (
-        <button onClick={() => addToWishlist(product)}>Add to wishList</button>
-      )}
-      {isInWishlist(_id) && (
-        <button onClick={() => removeFromWishlist(product._id, product)}>
-          Remove from wishlist
-        </button>
-      )}
+        {!isInWishlist(_id) && (
+          <button onClick={() => addToWishlist(product)}>
+            Add to wishList
+          </button>
+        )}
+        {isInWishlist(_id) && (
+          <button onClick={() => removeFromWishlist(product._id, product)}>
+            Remove from wishlist
+          </button>
+        )}
+      </div>
     </div>
   );
 };
--- my-app/src/components/DisplayWishlistProducts.js
+++ my-app/src/components/DisplayWishlistProducts.js
@@ -3,9 +3,10 @@ import { AppContext } from "../context/AppProvider";
 import { useNavigate } from "react-router-dom";
 
 export const DisplayWishlistProducts = ({ product }) => {
-  const { title, _id } = product;
+  const { title, _id, thumbnail } = product;
   const {
     addToCart,
+    removeFromCart,
     addToWishlist,
     removeFromWishlist,
     isInCart,
@@ -14,26 +15,35 @@ export const DisplayWishlistProducts = ({ product }) => {
   const navigate = useNavigate();
 
   return (
-    <div
-      style={{ border: "1px solid lightgray", margin: "10px", padding: "10px" }}
-    >
-      <h1>{title}</h1>
+    <div className="flex-row-space-evenly">
+      <img src={thumbnail}></img>
+      <div>
+        <h1>{title}</h1>
 
-      {!isInCart(_id) && (
-        <button onClick={() => addToCart(product)}>Add to cart</button>
-      )}
-      {isInCart(_id) && (
-        <button onClick={() => navigate("/cart")}>Go to cart</button>
-      )}
+        {!isInCart(_id) && (
+          <button onClick={() => addToCart(product)}>Add to cart</button>
+        )}
 
-      {!isInWishlist(_id) && (
-        <button onClick={() => addToWishlist(product)}>Add to wishList</button>
-      )}
-      {isInWishlist(_id) && (
-        <button onClick={() => removeFromWishlist(product._id, product)}>
-          Remove from wishlist
-        </button>
-      )}
+        {isInCart(_id) && (
+          <button onClick={() => removeFromCart(product._id, product)}>
+            Remove from cart
+          </button>
+        )}
+        {isInCart(_id) && (
+          <button onClick={() => navigate("/cart")}>Go to cart</button>
+        )}
+
+        {!isInWishlist(_id) && (
+          <button onClick={() => addToWishlist(product)}>
+            Add to wishList
+          </button>
+        )}
+        {isInWishlist(_id) && (
+          <button onClick={() => removeFromWishlist(product._id, product)}>
+            Remove from wishlist
+          </button>
+        )}
+      </div>
     </div>
   );
 };
