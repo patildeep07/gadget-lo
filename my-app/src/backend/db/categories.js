41d1d41c39c47a0e9d0e0a63317bcbdee6f2e155
Deep Patil <59699631+patildeep07@users.noreply.github.com>
2023-06-12 18:29:39 +0530

    Category box css updated

--- my-app/src/backend/db/categories.js
+++ my-app/src/backend/db/categories.js
@@ -9,24 +9,32 @@ export const categories = [
   {
     _id: uuid(),
     categoryName: "Mobile",
+    thumbnail:
+      "https://img.freepik.com/free-psd/full-screen-smartphone-mockup-design_53876-57925.jpg?size=626&ext=jpg&ga=GA1.2.353056144.1686079375&semt=sph",
     description:
       "Experience the world at your fingertips! Work, socialise, book a ride, play games, listen to music, watch your favourite shows, take a photo, or simply make a call! Buy a Mobile Phone from GadgetLo that does it all and then some.",
   },
   {
     _id: uuid(),
     categoryName: "Laptop",
+    thumbnail:
+      "https://img.freepik.com/free-photo/laptop-wooden-table_53876-20635.jpg?size=626&ext=jpg&ga=GA1.2.353056144.1686079375&semt=sph",
     description:
       "While tablets and smartphones are still popular, most people agree that everything, from doing research for an assignment to playing hardcore games, works better on a laptop. It doesn't matter what your lifestyle is, there is always one for you at GadgetLo.",
   },
   {
     _id: uuid(),
     categoryName: "Television",
+    thumbnail:
+      "https://img.freepik.com/free-vector/home-theater-with-tv-screen-speakers-modern-living-room-realistic-interior-with-plasma-television-hanging-wall-sound-stereo-system-stand-wooden-floor_107791-4323.jpg?size=626&ext=jpg&ga=GA1.1.353056144.1686079375&semt=sph",
     description:
       "Streaming services have made it possible to watch your favourite shows and live events on any screen you desire. But every so often you need a screen large enough so you can sink into your sofa or curl up in bed and watch your beloved stories come to life. It doesn't matter what your lifestyle is, there is always a TV for you at GadgetLo.",
   },
   {
     _id: uuid(),
     categoryName: "Accessories",
+    thumbnail:
+      "https://img.freepik.com/free-photo/ar-keyboard-hologram-neon-green-smart-technology-device_53876-165520.jpg?size=626&ext=jpg&ga=GA1.1.353056144.1686079375&semt=sph",
     description:
       "All add on items which you'd need along with your Laptops, mobiles, Tv's. Eg. Earphones, keyboard, mouse, charger, adapters, ect",
   },
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -17,12 +17,18 @@ export const Home = () => {
       <h1>Show all categories</h1>
       <div className="category-slide">
         {categoryData.map((category) => {
-          const { _id, categoryName, description } = category;
+          const { _id, categoryName, description, thumbnail } = category;
           return (
             <Link to="/store" key={_id}>
               <div
                 key={_id}
                 className="category-box"
+                style={{
+                  background: `url(${thumbnail})`,
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                }}
                 onClick={() =>
                   setAllData({
                     type: "UPDATE_CATEGORY_HOME",
@@ -30,8 +36,10 @@ export const Home = () => {
                   })
                 }
               >
-                <h3>{categoryName}</h3>
-                <p>{description}</p>
+                <div className="dark-overlay">
+                  <h3>{categoryName}</h3>
+                  <p>{description}</p>
+                </div>
               </div>
             </Link>
           );
